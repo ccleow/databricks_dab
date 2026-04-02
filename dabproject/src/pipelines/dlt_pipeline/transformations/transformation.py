667bc7eed521c1dd13956b6634b1cbdadc539181
ccleow <ccleow@gmail.com>
2026-04-02 08:04:25 +0000

    add pipeline

---
+++ dabproject/src/pipelines/dlt_pipeline/transformations/transformation.py
@@ -0,0 +1,5 @@
+import dlt
+
+@dlt.table
+def transformed():
+    return spark.range(10)https://adb-7405612105794787.7.azuredatabricks.net/browse/folders/2041951855756130?contextId=pipeline%3Ae78d2b1b-57b0-46af-8592-476aa0480f99&o=7405612105794787$0
